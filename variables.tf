f19a13f384d6ede953be085bcff017fa6064c5d2
yuvan11 <yuva.ram11@gmail.com>
2022-07-31 19:40:11 +0530

    adding variables

---
+++ variables.tf
@@ -0,0 +1,18 @@
+variable "filename" {
+    default = "/home/yuvi/terraform-practice/pets.txt"
+}
+
+variable "content" {
+    default = "I love pets"
+}
+
+variable "prefix" {
+  default =  "Mrs"
+}
+variable "separator" {
+  default = "."
+}
+
+variable "length" {
+  default = 1
+}
